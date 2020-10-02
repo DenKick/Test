e96378b0305a3734812e77ffd284684d13d265e4
Denis Brester <49287397+DenKick@users.noreply.github.com>
2020-10-02 19:52:09 +0300

    Fixed some issues in mobile version

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 let headerMenu = document.getElementById("header__menu");
+let header = document.getElementById("header");
 
 openMenu = () => {
   headerMenu.classList.add("opened");
+  header.classList.remove("closed-header");
+  header.classList.add("opened-header");
   headerMenu.setAttribute("onclick", "closeMenu()")
 }
 
 closeMenu = () => {
   headerMenu.classList.remove("opened");
+  header.classList.remove("opened-header");
+  header.classList.add("closed-header");
   headerMenu.setAttribute("onclick", "openMenu()")
 }
